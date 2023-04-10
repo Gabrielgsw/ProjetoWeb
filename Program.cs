387c05a3595c90bc9b2242d9a7f6824fcf8dd068
Gabrielgsw <ggermanow279@gmail.com>
2023-04-10 16:36:08 -0300

    commit 4

--- Program.cs
+++ Program.cs
@@ -15,7 +15,12 @@ public class Program
             {
             contexto.Response.Redirect("produtos.html", false);
             });
-        app.MapGet("/clientes" , () => "Teste de Clientes");
+        //app.MapGet("/clientes" , () => "Teste de Clientes");
+        app.MapGet("/clientes",(string nome, string email) =>$"O nome do cliente escolhido é: {nome} \n O email do cliente é: {email}");
+        Pessoa p1=new Pessoa() {id=1, nome="Gabriel"};
+
+        app.MapGet("/fornecedores" , () =>
+            $"O fornecedo é: {p1.id} - {p1.nome}");
         app.Run();
     }
 }
