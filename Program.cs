035c99a544c96be498df0b2b704c020c004e7af6
Gabrielgsw <ggermanow279@gmail.com>
2023-04-10 16:24:11 -0300

    commit 3

--- Program.cs
+++ Program.cs
@@ -10,7 +10,12 @@ public class Program
         app.UseStaticFiles();
         app.MapGet("/", () => "Hello World!");
         app.MapGet("/cliente", () => "Cliente !!!!");
-        app.MapGet("/produtos", ()=> "Produtos!!!!");
+        //app.MapGet("/produtos", ()=> "Produtos!!!!");
+        app.MapGet("/produtos", (HttpContext contexto) =>
+            {
+            contexto.Response.Redirect("produtos.html", false);
+            });
+        app.MapGet("/clientes" , () => "Teste de Clientes");
         app.Run();
     }
 }
